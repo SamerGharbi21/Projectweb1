7bdc863fa6e2fcf3dc6ec007a63f61f9cc0b2f15
SamerGharbi21 <Gharbisamer2@gmail.com>
2023-11-25 22:36:02 +0300

    done

--- index.php
+++ index.php
@@ -47,6 +47,7 @@ if (isset($_POST['add_product'])) {
     }
 }
 
+
 $query = "SELECT * FROM products";
 $result = mysqli_query($conn, $query);
 
@@ -61,49 +62,75 @@ if ($result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="di.css" >
+    <link rel="stylesheet" href="./css/di.css" >
     <title>Document</title>
 </head>
 <body>
-<form class="mt-5 text-center" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-                                <h3> Product Information </h3>
+    <!-- Navigation Bar -->
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="orders.php">Orders</a>
+        <a href="cart.php">Cart</a>
+        <a href="login.php">Login</a>
+    </div>
 
-   <div class="mb-3">
-    <label for="pname" class="form-label">Product name</label>
-    <input type="text" class="form-control" name="pname" id="pname" placeholder= "Enter Product name" value="<?php echo $pname?>">
-  </div>
+    <form class="mt-5 text-center" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+    <h3> Product Information </h3>
 
-  <div class="mb-3">
-    <label for="Pprice" class="form-label">Product Price</label>
-    <input type="text" class="form-control" name="Pprice" id="Pprice" placeholder= "Enter Product Price" value="<?php echo $Pprice?>">
-  </div>
+    <div class="mb-3">
+        <div>
+            <label for="pname" class="form-label">Product name</label>
+        </div>
+        <div>
+            <input type="text" class="form-control" name="pname" id="pname" placeholder="Enter Product name" value="<?php echo $pname ?>">
+        </div>
+    </div>
 
+    <div class="mb-3">
+        <div>
+            <label for="Pprice" class="form-label">Product Price</label>
+        </div>
+        <div>
+            <input type="number" class="form-control" name="Pprice" id="Pprice" placeholder="Enter Product Price" value="<?php echo $Pprice ?>">
+        </div>
+    </div>
 
-  <div class="mb-3">
-    <label for="Pquantity" class="form-label">Product Quantity</label>
-    <input type="number" class="form-control" name="Pquantity" id="Pquantity" placeholder= "Enter Product Price" value="<?php echo $Pquantity?>">
-  </div>
+    <div class="mb-3">
+        <div>
+            <label for="Pquantity" class="form-label">Product Quantity</label>
+        </div>
+        <div>
+            <input type="number" class="form-control" name="Pquantity" id="Pquantity" placeholder="Enter Product Quantity" value="<?php echo $Pquantity ?>">
+        </div>
+    </div>
 
+<div class="mb-3">
+        <div>
+            <label for="Pdetail" class="form-label">Product Details</label>
+        </div>
+        <div>
+            <input type="text" class="form-control" name="Pdetail" id="Pdetail" placeholder="Enter Product Details" value="<?php echo $Pdetail ?>">
+        </div>
+    </div>
 
-
-  <div class="mb-3 my-3">
+    <div class="mb-3">
+        <div>
             <label for="Pimage" class="form-label">Product Image</label>
+        </div>
+        <div>
             <input type="file" class="form-control" name="Pimage" id="Pimage" value="<?php echo $Pimage ?>">
         </div>
+    </div>
 
+    
 
-    <div class="mb-3 my-3">
-    <label for="Pdetail" class="form-label">Product Details</label>
-    <input type="text" class="form-control" name="Pdetail" id="Pdetail" placeholder= "Enter Product Details" value="<?php echo $Pdetail?>">
-  </div>
-
-
-  </div>
-  <button type="submit" name="add_product" value="add product"calss="btn">Add Product</button>
+    <button type="submit" name="add_product" value="add product" class="btn">Add Product</button>
 </form>
 
+
+
 <div class="mt-5 text-center">
-        <h3> All Products </h3>
+        <h3 id ="pro"> All Products </h3>
         <table>
             <tr>
                 <th>Name</th>
@@ -118,7 +145,7 @@ if ($result) {
                 echo "<tr>";
                 echo "<td>{$product['name1']}</td>";
                 echo "<td> $ {$product['price']}</td>";
-                echo "<td>{$product['quantity']}</td>";
+                echo "<td>   {$product['quantity']}</td>";
                 echo "<td>{$product['description1']}</td>";
                 echo "<td><img src='{$product['image1']}' alt='Product Image' style='max-width: 100px; max-height: 100px;'></td>";
                 echo "</tr>";
